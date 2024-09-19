92903778e1abf5bb02d14e9689b4a9b52199fe46
Adebayo Adekunle <122753023+TechBroK@users.noreply.github.com>
2024-09-19 21:42:19 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1> My goal for Today</h1>
+    <ul>
+      <li>react.js</li>
+      <li>presentation package</li>
+      <li>pray</li>
+    </ul>
+  </div>,
+
+  document.getElementById("root")
+);
